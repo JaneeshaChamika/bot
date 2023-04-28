472868f40484b301a872154ed2f4ca7fa79ea7b8
JaneeshaChamika <110347019+JaneeshaChamika@users.noreply.github.com>
2023-04-28 08:36:18 +0530

    Update config.py

--- bot/config.py
+++ bot/config.py
@@ -2,30 +2,30 @@ import os
 
 class Config:
 
-    BOT_TOKEN = ""
+    BOT_TOKEN = "6015454888:AAG8tDGhbKUz2rkwHJsvBhNXs_9YCkL8sgg"
 
-    SESSION_NAME = ""
+    SESSION_NAME = "upload_Pro_X_bot"
 
-    API_ID = ""
+    API_ID = "16476797"
 
-    API_HASH = ""
+    API_HASH = "ce944343fb868da0cb85d555d6db1c69"
 
-    CLIENT_ID = ""
+    CLIENT_ID = "805240374465-auj968seia5hh0rgqjgjf797152vrm5s.apps.googleusercontent.com"
 
-    CLIENT_SECRET = ""
+    CLIENT_SECRET = "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"
 
-    AUTH_USERS = [942731625]
+    AUTH_USERS = [5502817142]
 
-    VIDEO_DESCRIPTION = ""
+    VIDEO_DESCRIPTION = "upload pro X"
 
-    VIDEO_CATEGORY = ""
+    VIDEO_CATEGORY = "educatio"
 
-    VIDEO_TITLE_PREFIX = ""
+    VIDEO_TITLE_PREFIX = "hi"
 
-    VIDEO_TITLE_SUFFIX = ""
+    VIDEO_TITLE_SUFFIX = "last"
     
     DEBUG = bool()
 
-    UPLOAD_MODE = "unlisted"
+    UPLOAD_MODE = "privet"
     
     CRED_FILE = "auth_token.txt"
